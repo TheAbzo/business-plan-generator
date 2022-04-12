acd4c170c4ce3120534571c284c4dc109bcf083e
theAbzo <soon-ab@hotmail.com>
2022-04-12 15:19:02 +0200

    finishing states changing effects on choices

--- src/App.js
+++ src/App.js
@@ -12,17 +12,32 @@ function App() {
 
 
     // radio button choices, disabled, id, default choice
-    const [data, setData] = useState({"Is your salary above 1000$?":["yes","no",false,0],
-        "Is your salary above 2000?":["yes","no",false,1,"yes"],
-        "Is your salary above 3000?":["yes","no",false,2,"yes"],
-        "Is your salary above 4000?":["yes","no",false,3,"yes"],
+    const [data, setData] = useState({"Is your salary above 1000$?":["yes","no",false,0, "yes"],
+        "Is your bank balance above 70k $?":["yes","no",false,1,"yes"],
+        "Do you have any debts?":["yes","no",false,2,"yes"],
+        "Do you have any assests?":["yes","no",false,3,"yes"],
         "Is your salary above 5000?":["yes","no",false,4,"yes"]});
+    const [, forceRender] = useState({});
 
     function changeState(id, val) {
-        data[id] = val
-        setData(data)
+        console.log(data[id][4], "hhhsahah")
+        data[id][4] = val
+
         console.log("app updated")
         console.log(data)
+
+        if(id === "Do you have any assests?" && val === "no"){
+            console.log("we disable last question")
+            console.log(data[id][2],"dsadasdddadshasaaa")
+            data["Is your salary above 5000?"][2] = true
+            console.log(data)
+        }else {
+            data["Is your salary above 5000?"][2] = false
+        }
+
+        console.log(data[id][2],"dsadasdddadshasaaa")
+        setData(data);
+        forceRender({});
     }
     const sliceAt = 3;
     const dataArr = Object.entries(data);
--- src/Section.js
+++ src/Section.js
@@ -10,8 +10,10 @@ function Section(props){
         props.changeState(keyD,valueD)
         console.log("section updated")
     }
-
+    let x = Object.entries(QA)
     let listItem =    Object.entries(QA)
+            .filter(([name, answers]) => {
+                if(answers[3] == 2 && x[0][1][4] == "no") return false; return [name, answers];} )
             .map( ([name, answers]) =>
                 <MDBListGroupItem key = {name}><Question question = {name}
                               answer1 = {answers[0]}
@@ -22,8 +24,12 @@ function Section(props){
                 />
                 </MDBListGroupItem> );
 
-
-    console.log(listItem)
+    //filter gray items, if answer of 4th question is No, 5th question is greyed
+    // console.log(x[0][1][4],"hello")
+    if (x[0][1][4] == "no"){
+        console.log(x[0][1][4],"helooooooooooooo")
+    }
+    // console.log(listItem)
     return(
         <MDBListGroup className={"mid-button-child"}>
             {listItem}
